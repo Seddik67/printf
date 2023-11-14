20754795f84cdf7dd1008139ffcd17eb33a7fe84
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-14 06:10:59 -0800

    Ldljds

--- print_string.c
+++ print_string.c
@@ -1,34 +1,29 @@
 #include "main.h"
-
 /**
- * print_s - function to print a string
- * @val: argument
- *
- * Return: the length of the string
+ * print_s - print a string.
+ * @val: argumen t.
+ * Return: the length of the string.
  */
 
 int print_s(va_list val)
 {
 	char *s;
-	int i, length;
+	int i, len;
 
 	s = va_arg(val, char *);
-
 	if (s == NULL)
 	{
-		s = "(nil)";
-		length = _strlen(s);
-
-		for (i = 0; i < length; i++)
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
 			_putchar(s[i]);
-		return (length);
+		return (len);
 	}
 	else
 	{
-		length = _strlen(s);
-
-		for (i = 0; i < length; i++)
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
 			_putchar(s[i]);
-		return (length);
+		return (len);
 	}
 }
