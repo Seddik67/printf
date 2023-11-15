22ef924cde541c04d1edb3815a10a8e5be47df09
Seddik67 <mohamedseddikqwe@gmail.com>
2023-11-15 09:01:30 -0800

    sljd

--- _printf.c
+++ _printf.c
@@ -26,8 +26,6 @@ int _printf(const char *format, ...)
 	int length = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
 	while (format[i] != '\0')
 	{
